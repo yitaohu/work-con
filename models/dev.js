bd1b5992f23f2270df137f3012aac0d34b560e40
yitaohu <huyitao1987@gmail.com>
2017-12-07 14:33:38 -0500

    explain

--- models/dev.js
+++ models/dev.js
@@ -8,13 +8,17 @@ var Dev={
         path = run1Path;
 
         PathProc.getFullTestResultPath(TestNameArray,path, function(err,res) {
-            console.log(res);
+            //res example
+            // [ { slab_rad: 'file://lebqa01.ansys.com/users/yihu/yihu/web/Result2/fluent/slab_rad/v19.0.0/slab_rad.short.dp.lnamd64-t4.17_12_06_11_08_51' },   { rep_def_cff: 'file://lebqa01.ansys.com/users/yihu/yihu/web/Result2/fluent/rep_def_cff/v19.0.0/rep_def_cff.short.dp.lnamd64-t4.17_12_06_14_34_19' } ]
         });
 
-        var convNum = Convergence.getConvNum(path,function(err,data) {
+        Convergence.getConvNum(path,function(err,data) {
 
-        });
-        return convNum;
+        });// 每一个上面的path 都要调用一遍他 得到后面的数值
+        //最后得到 [ { slab_rad: "15"},   { rep_def_cff: "200" } ]
+
+        
+        
     }
 };
 module.exports=Dev;
