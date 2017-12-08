578856c137ebb7e546b30371ba7414052cd00e21
yitaohu <huyitao1987@gmail.com>
2017-12-08 17:25:40 -0500

    plot works

--- assets/app/plot/plot.component.ts
+++ assets/app/plot/plot.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges, OnInit } from '@angular/core';
+
 
 @Component({
  selector: 'app-line-chart',
  templateUrl: './plot.component.html'
 })
-export class PlotComponent implements OnChanges {
+export class PlotComponent implements OnChanges  {
 
  @Input() showPlot: Boolean;
  @Input() plotData;
@@ -15,9 +16,18 @@ ngOnChanges(changes: SimpleChanges){
   for (let propName in changes) {
     let chng = changes[propName];
     console.log(chng);
+    {
+      let _lineChartData:Array<any> = new Array(this.lineChartData.length);
+      for (let i = 0; i < this.lineChartData.length; i++) {
+        _lineChartData[i] = {data: new Array(this.lineChartData[i].data.length), label: this.lineChartData[i].label};
+        for (let j = 0; j < this.lineChartData[i].data.length; j++) {
+          _lineChartData[i].data[j] = this.plotData[j];
+        }
+      }
+      this.lineChartData = _lineChartData;
+    }
     let cur  = JSON.stringify(chng.currentValue);
     let prev = JSON.stringify(chng.previousValue);
-    this.changeLog.push(`${propName}: currentValue = ${cur}, previousValue = ${prev}`);
   }
 }
 
@@ -27,7 +37,7 @@ ngOnChanges(changes: SimpleChanges){
   //  {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'},
   //  {data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C'}
  ];
- public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+ public lineChartLabels:Array<any> = ['January', 'February'];
  public lineChartOptions:any = {
    responsive: true
  };
