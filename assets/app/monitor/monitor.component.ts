de402c367a8407544f8d14fc9011632af310af0e
yitaohu <huyitao1987@gmail.com>
2018-05-24 17:03:54 -0400

    failed test command corrected done

--- assets/app/monitor/monitor.component.ts
+++ assets/app/monitor/monitor.component.ts
@@ -27,6 +27,7 @@ export class MonitorComponent implements OnInit {
     Object = Object;
     resutlstring={};
     isResultReady = false;
+    command = "";
 
     def_version ;
     def_testListPath ;
@@ -93,43 +94,82 @@ export class MonitorComponent implements OnInit {
                     console.log(data);
                     this.resutlstring = data;
                     this.isResultReady = true;
+                    
 
                 },
                 error => console.error(error)
             )
     }
 
-    // dataProcess(data:Object[]) {
-    //     var listName = [];
-    //     var convNum = [];
-    //     var sortable = [];
-    //     var run1conv = [];
-    //     var run2conv = [];
-    //     var outDir1 = [];
-    //     var outDir2 = [];
-    //     for (var testItem in data) {
-    //         sortable.push([testItem, data[testItem][2],data[testItem][0],data[testItem][1], data[testItem][3], data[testItem][4]]);
-    //     }
+    copyToClipboard() {
+        this.command=this.getAllCommand(this.resutlstring);
+        var textArea = document.createElement("textarea");
+        textArea.style.position = 'fixed';
+        textArea.style.top = "0";
+        textArea.style.left = "0";
+        textArea.style.width = '2em';
+        textArea.style.height = '2em';
+        textArea.style.padding = "0";
+        textArea.style.border = 'none';
+        textArea.style.outline = 'none';
+        textArea.style.boxShadow = 'none';
+        textArea.style.background = 'transparent';
+        textArea.value = this.command;
+
+        document.body.appendChild(textArea);
+        textArea.focus();
+        textArea.select();
+        try {
+            var successful = document.execCommand('copy');
+            var msg = successful ? 'successful' : 'unsuccessful';
+            console.log('Copying text command was ' + msg);
+        } catch (err) {
+            console.log('Oops, unable to copy');
+        }
+
+        document.body.removeChild(textArea);
         
-    //     sortable.sort(function(a, b) {
-    //         return b[1] - a[1];
-    //     });
-    //     sortable.forEach(function(Element) {
-    //         testName.push(Element[0]);
-    //         convNum.push(Element[1]);
-    //         run1conv.push(Element[2]);
-    //         run2conv.push(Element[3]);
-    //         outDir1.push(Element[4]);
-    //         outDir2.push(Element[5]);
+      }
 
-    //     })
+    getAllCommand(data:Object) {
+        console.log(data);
+        var listName = [];
+        var testname = [];
+        var sortable = [];
+        var run1conv = [];
+        var run2conv = [];
+        var outDir1 = [];
+        var outDir2 = [];
+        for (var listItem in data) {
+            for (var testItem in data[listItem]) {
+                if((data[listItem][testItem].length != 0)) {
+                    sortable = sortable.concat(data[listItem][testItem]);
+                }
+                
+            }
+        }
+        console.log(sortable);
+        return sortable.join('\n')
+        
+        // sortable.sort(function(a, b) {
+        //     return b[1] - a[1];
+        // });
+        // sortable.forEach(function(Element) {
+        //     testName.push(Element[0]);
+        //     convNum.push(Element[1]);
+        //     run1conv.push(Element[2]);
+        //     run2conv.push(Element[3]);
+        //     outDir1.push(Element[4]);
+        //     outDir2.push(Element[5]);
 
-    //     return [testName,
-    //             convNum,
-    //             run1conv,
-    //             run2conv,
-    //             outDir1,
-    //             outDir2
-    //         ];
-    // }
+        // })
+
+        // return [testName,
+        //         convNum,
+        //         run1conv,
+        //         run2conv,
+        //         outDir1,
+        //         outDir2
+        //     ];
+    }
 }
