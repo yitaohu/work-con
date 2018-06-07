b7e1d4d5490003438228110881b8104f9499a9c5
yitaohu <huyitao1987@gmail.com>
2018-06-07 16:14:39 -0400

    change tab name and change default value

--- assets/app/header.component.ts
+++ assets/app/header.component.ts
@@ -5,8 +5,8 @@ import { Component } from '@angular/core';
         <header class="row">
             <nav class="col-md-8 col-md-offset-2">
                  <ul class="nav nav-pills">
-                    <li routerLinkActive="active"><a [routerLink]="['/dev']">Developer</a></li>
-                    <li routerLinkActive="active"><a [routerLink]="['/testing']">Tester</a></li>
+                    <li routerLinkActive="active"><a [routerLink]="['/dev']">Local Std Build</a></li>
+                    <li routerLinkActive="active"><a [routerLink]="['/testing']">Database Std Build</a></li>
                     <li routerLinkActive="active"><a [routerLink]="['/monitor']">Monitor</a></li>
                  </ul>
             </nav>
